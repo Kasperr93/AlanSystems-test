bfc0ff06af6916b4a49c6aa1e4f1eb859c750b33
Tomasz Kasperek <kasperek1993@gmail.com>
2019-07-02 10:07:59 +0200

    - initial commit;

---
+++ src/main/java/com/alansystems/bookmaking/BookmakingApplication.java
@@ -0,0 +1,12 @@
+package com.alansystems.bookmaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmakingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmakingApplication.class, args);
+    }
+}
